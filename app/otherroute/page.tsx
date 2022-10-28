78e47fae6cb1b8ce262bf519c8afb9ce4a188b3b
Helge Ahrens <helgeahrens@Helges-MacBook-Pro.local>
2022-10-28 18:24:58 +0200

    wat

--- app/otherroute/page.tsx
+++ app/otherroute/page.tsx
@@ -1,6 +1,6 @@
 import { getRandom } from '../../lib/util';
 export const revalidate = 0;
-const OtherPage = () => {
+const OtherPage = async () => {
   return <div>Other page random: {getRandom()}</div>;
 };
 export default OtherPage;
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,10 @@
+import { cookies, headers } from 'next/headers';
 // export default function MyPage() {
 
 import { getRandom } from '../../lib/util';
-export const revalidate = 0;
-const MyPage = () => {
+// export const revalidate = 1;
+export default async function Page() {
+  const bla = await fetch('http://localhost:3000/', { cache: 'no-store' });
+  console.log(headers().entries());
   return <div>Test Page Random: {getRandom()}</div>;
-};
-
-export default MyPage;
+}
