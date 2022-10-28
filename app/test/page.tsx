b561da9b368de07f2145b359555f5c1e2fbb92b1
Helge Ahrens <helgeahrens@Helges-MacBook-Pro.local>
2022-10-28 18:28:35 +0200

    waat

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -4,7 +4,7 @@ import { cookies, headers } from 'next/headers';
 import { getRandom } from '../../lib/util';
 // export const revalidate = 1;
 export default async function Page() {
-  const bla = await fetch('http://localhost:3000/', { cache: 'no-store' });
+  const bla = await fetch('https://google.com', { cache: 'no-store' });
   console.log(headers().entries());
   return <div>Test Page Random: {getRandom()}</div>;
 }
